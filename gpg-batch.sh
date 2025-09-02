e248d2260a8e736faacb12c3032daa07d19431ba
Semyon A Mironov <s.mironov@mgmsam.pro>
2025-09-02 05:10:03 +0400

    gpg-batch.sh: Unattended key generation
    Signed-off-by: Semyon A Mironov <s.mironov@mgmsam.pro>

---
+++ gpg-batch.sh
@@ -0,0 +1,134 @@
+#!/bin/sh
+# gpg-batch. Unattended key generation.
+#
+# Copyright (c) 2025 Semyon A Mironov
+#
+# Authors: Semyon A Mironov <s.mironov@mgmsam.pro>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+if test "${KSH_VERSION:-}"
+then
+    PUTS=print
+    puts ()
+    {
+        print "${PUTS_OPTIONS:--r}" -- "$*"
+    }
+else
+    if type printf >/dev/null 2>&1
+    then
+        PUTS=printf
+        puts ()
+        {
+            printf "${PUTS_OPTIONS:-%s\n}" "$*"
+        }
+    elif type echo >/dev/null 2>&1
+    then
+        PUTS=echo
+        puts ()
+        {
+            echo "${PUTS_OPTIONS:-}" "$*"
+        }
+    else
+        exit 1
+    fi
+fi
+
+say ()
+{
+    RETURN=$?
+    PUTS_OPTIONS=
+    while test $# -gt 0
+    do
+        case "${1:-}" in
+            -n)
+                test "$PUTS" = printf &&
+                PUTS_OPTIONS=%s ||
+                PUTS_OPTIONS=-n
+                ;;
+            *[!0-9]*|"")
+                break
+                ;;
+            *)
+                RETURN=$1
+        esac
+        shift
+    done
+    case "$@" in
+        ?*)
+            puts "$PKG:${1:+" $@"}"
+            PUTS_OPTIONS=
+    esac
+}
+
+die ()
+{
+    say "$@" >&2
+    exit "$RETURN"
+}
+
+which ()
+{
+    case "${PATH:-}" in
+        *[!:]:)
+            PATH="$PATH:"
+            ;;
+    esac
+    RETURN=1
+    case "${1:-}" in
+        */*)
+            if  test -f "$1" &&
+                test -x "$1"
+            then
+                puts "$1"
+                RETURN=0
+            fi
+            ;;
+        ?*)
+            IFS_SAVE="${IFS:-}"
+            IFS=:
+            for ELEMENT in $PATH
+            do
+                test "${ELEMENT:-}" || ELEMENT=.
+                if  test -f "$ELEMENT/$1" &&
+                    test -x "$ELEMENT/$1"
+                then
+                    puts "$ELEMENT/$1"
+                    RETURN=0
+                    break
+                fi
+            done
+            IFS="${IFS_SAVE:-}"
+    esac
+    return "$RETURN"
+}
+
+gpg_generate ()
+{
+    cat "$1" | "$GPG" $GPG_OPTIONS --full-gen-key
+}
+
+main ()
+{
+    PKG="${0##*/}"
+    GPG="$(which gpg)" || die "gpg: command not found"
+    GPG_OPTIONS="--batch --expert --command-fd=0 --status-fd=1 --pinentry-mode=loopback --verbose"
+
+    for BATCH in "$@"
+    do
+        gpg_generate "$BATCH"
+    done
+}
+
+main "$@"
